5a99df8211f7008cd9dd3ef0b5a663fc5ed68757
nate-adkins <nathanpadkins@gmail.com>
2025-10-30 01:29:43 -0400

    alignment

--- helpers.py
+++ helpers.py
@@ -1,23 +1,22 @@
 from pyglet import shapes
-from math import sin, cos
+from math import sin, cos, atan2
 
 class BoidShape():
-    def __init__(self, x, y, theta, batch):
+    def __init__(self, x, y, theta, color, batch):
         self.length = 15
-        self._circle = shapes.Circle(x, y, radius=5, segments=20, color=(64,128,128), batch=batch)
+        self._circle = shapes.Circle(x, y, radius=5, segments=20, color=color, batch=batch)
         x2 = x + self.length * cos(theta)
         y2 = y + self.length * sin(theta)
-        self._line = shapes.Line(x, y, x2, y2, 3, color=(64,128,128), batch=batch)
+        self._line = shapes.Line(x, y, x2, y2, 3, color=color, batch=batch)
 
-    def update_pos(self, dx, dy, new_theta):
+    def update_pos(self, dx, dy):
         self._circle.x += dx
         self._circle.y += dy
 
-        x = self._circle.x
-        y = self._circle.y
-        
-        self._line.x = x
-        self._line.y = y
-        self._line.x2 = x + self.length * cos(new_theta)
-        self._line.y2 = y + self.length * sin(new_theta)
+        new_theta = atan2(dy,dx)
+
+        self._line.x = self._circle.x
+        self._line.y = self._circle.y
+        self._line.x2 = self._circle.x + self.length * cos(new_theta)
+        self._line.y2 = self._circle.y + self.length * sin(new_theta)
 
--- sim.py
+++ sim.py
@@ -24,9 +24,11 @@ batch = pyglet.graphics.Batch()
 boids_array = np.empty(0,dtype=boid_type)
 shape_array: list[BoidShape] = []
 
-def calculate_centroid():
+def calc_centroid():
     return (np.mean(boids_array['x']),np.mean(boids_array['y']))
 
+def calc_avg_head():
+    return np.mean(boids_array['theta'])
 
 for n in range(BOIDS):
     new_theta = random.random()*360
@@ -35,45 +37,51 @@ for n in range(BOIDS):
     new_boid = np.array([(new_theta,new_x,new_y)],dtype=boid_type)
     boids_array = np.concatenate([boids_array,new_boid])
     
-    shape_array.append(BoidShape(new_x,new_y,new_theta,batch))
+    shape_array.append(BoidShape(new_x,new_y,new_theta,(64,128,128),batch))
 
-centroid = calculate_centroid()
-centroid_shape = shapes.Circle(centroid[0],centroid[1],radius=10,segments=20, color=(255,0,0), batch=batch)
+centroid = calc_centroid()
+centroid_boid = BoidShape(centroid[0],centroid[1],0,(255,0,0),batch)
 
 def update(hz):
 
-    # update numpy arrays using boids rules xt+1 = Bboids(xt)
-    # update visuals 
-    centroid = calculate_centroid()
-    centroid_shape.x = centroid[0]
-    centroid_shape.y = centroid[1]
+    centroid = calc_centroid()
+    avg_heading = calc_avg_head()
 
+    centroid_boid.update_pos(centroid[0],centroid[1])
     rand_strength = 0
     cohesion_strength = 2
+    alginment_strength = 4
+
     for i, boid in enumerate(boids_array):
 
-        rand_dx = random.gauss() * rand_strength
-        rand_dy = random.gauss() * rand_strength
+        if rand_strength != 0: 
+            rand_dx, rand_dy = (random.gauss(0,1), random.gauss(0,1)) * rand_strength
+        else: # skip gaussian sampling
+            rand_dx, rand_dy = 0,0
 
+        # cohesion
         angle_to_centroid = atan2((centroid[1] - boid['y']),(centroid[0] - boid['x']))
         cohesion_dx = cos(angle_to_centroid) * cohesion_strength
         cohesion_dy = sin(angle_to_centroid) * cohesion_strength
 
-        dx = rand_dx + cohesion_dx
-        dy = rand_dy + cohesion_dy
-        new_theta = atan2(dy,dx)
+        # alignment 
+        alignment_dx = cos(avg_heading) * alginment_strength
+        alignment_dy = sin(avg_heading) * alginment_strength
+
+        dx = rand_dx + cohesion_dx + alignment_dx
+        dy = rand_dy + cohesion_dy + alignment_dy
 
         boid['x'] += dx
         boid['y'] += dy
-        boid['theta'] = new_theta
+        boid['theta'] = atan2(dy,dx)
 
-        shape_array[i].update_pos(dx,dy,new_theta)
+        shape_array[i].update_pos(dx,dy)
 
 @win.event
 def on_draw():
     win.clear()
     batch.draw()
 
-pyglet.clock.schedule_interval(update, 1/10.0)
+pyglet.clock.schedule_interval(update, 1/60.0)
 
 pyglet.app.run()
